2277ff56a72487d758781f2e45fc6c1309b4f04d
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-07-19 11:18:34 +0300

    Fix serial commit bug

--- serial/move.inc.cpp
+++ serial/move.inc.cpp
@@ -16,7 +16,7 @@ int main(int argc, char **argv){
     ex_atoi(argv[2]);
     try{
         PortController ser("\\\\.\\COM"+to_string(getPortIni()));
-        ser.writeData("ORDER 201 " + string(argv[1]) + " " + string(argv[2]) + " 8193 50 50 0 0 0 0\r\nMOVE 201");
+        ser.writeData("ORDER 201 " + string(argv[1]) + " " + string(argv[2]) + " 8193 100 100 0 0 0 0\r\nMOVE 201\r\n");
     } catch (PortControllerException err){
         cerr << err.getMessage();
     }
