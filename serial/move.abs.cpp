ca7681b8105de5a1f55d4636a4d56fe2e01fd538
artun-akdogan <artun.akdogan@boun.edu.tr>
2022-07-19 15:40:27 +0300

    Add acceleration set for X-Y axis

--- serial/move.abs.cpp
+++ serial/move.abs.cpp
@@ -7,16 +7,17 @@
 using namespace std;
 
 int main(int argc, char **argv){
-    // First parameter is position, second parameter is speed
-    if(argc<3){
-        cerr << "This program works with at least 2 arguments!" << endl;
+    // First parameter is position, second parameter is speed, third parameter is acceleration
+    if(argc<4){
+        cerr << "This program works with at least 3 arguments!" << endl;
         return 0;
     }
     ex_atoi(argv[1]);
     ex_atoi(argv[2]);
+    ex_atoi(argv[3]);
     try{
         PortController ser("\\\\.\\COM"+to_string(getPortIni()));
-        ser.writeData("ORDER 201 " + string(argv[1]) + " " + string(argv[2]) + " 8192 100 100 0 0 0 0\r\nMOVE 201\r\n");
+        ser.writeData("ORDER 201 " + string(argv[1]) + " " + string(argv[2]) + " 8192 " + string(argv[3])+" " + string(argv[3]) +" 0 0 0 0\r\nMOVE 201\r\n");
     } catch (PortControllerException err){
         cerr << err.getMessage();
     }
